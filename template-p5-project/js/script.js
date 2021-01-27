f3627bc96910c67615508d6f516ba9232b8b30ae
ShandonFleming948 <shandonflem@gmail.com>
2021-01-27 15:48:06 -0400

    Added constants to script.js A1

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -11,6 +11,12 @@ author, and this description to match your project!
 /**
 Description of preload
 */
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
 function preload() {
 
 }
