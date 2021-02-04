64cb2faf6c2a421e07daf8a846469706f3a108a3
ShandonFleming948 <shandonflem@gmail.com>
2021-02-04 11:58:34 -0400

    Added animal array and reverse string function

---
+++ exercises/forget/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+Where’s Sausage Dog? New Game+
+Shandon Fleming
+
+-this code displays a variety of animals on the user's screen
+-out of the variety of animals, the user must locate the sausage dog before the timer runs out in order to win
+*/
