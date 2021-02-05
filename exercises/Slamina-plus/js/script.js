04a868274afba309bfc5e7b36523db67c52e4997
ShandonFleming948 <shandonflem@gmail.com>
2021-02-04 23:41:47 -0400

    Added final comments

--- exercises/Slamina-plus/js/script.js
+++ exercises/Slamina-plus/js/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+Slamina-plus
+by: Shandon Fleming
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This cpde displays a simulation in which the user has to decipher the name of a given vehicle brand. If the user is correct, the answer will appear in green. If the user is wrong, your answer will appear in red.
 */
+
+//array of vehicle names
 const words = [
     "Aston Martin",
     "Audi",
@@ -45,23 +46,19 @@ const words = [
     "Volvo"
     ];
 
+//created variables for the word provided by the simulation and the word provided by the user
     let currentWord = ``;
     let currentAnswer = ``;
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
+//pop up alert to provide the user with instructions for the simulation
+    alert(`Decipher the name of the vehicle brand that is given to you. If you are correct, your answer will appear in green. If you are wrong, your answer will appear in red. `);
 
 
-/**
-Description of setup
-*/
 function setup() {
+ //canvas covers the whole screen
  createCanvas(windowWidth, windowHeight);
 
+ //conditional for voice recognition
  if (annyang) {
    let commands = {
      'I think it is *word': guessWord
@@ -69,7 +66,8 @@ function setup() {
    annyang.addCommands(commands);
    annyang.start();
 
-   textSize(32);
+ //properties of the text
+   textSize(62);
    textStyle(BOLD);
    textAlign(CENTER, CENTER);
  }
@@ -77,26 +75,31 @@ function setup() {
 
 
 /**
-Description of draw()
+if the user's answer is correct, the word will be displayed in red/if wrong - green
 */
 function draw() {
-  background(0);
+  background(230, 239, 255);
 
+//correct answer - green
   if (currentAnswer === currentWord) {
-    fill(0, 255, 0);
+    fill(36, 240, 43);
   }
+//incorrect answer - red
   else {
-    fill(255, 0, 0);
+    fill(240, 36, 36);
   }
+//displays the user's answer in the center of the screen
   text(currentAnswer, width/2, height/2);
 }
 
+//if the mouse is pressed, the computer will provide the user with a reversed word
 function mousePressed() {
-  currentWord = random(words);
+  currentWord = random(words).toLowerCase();
   let reverseWord = reverseString(currentWord);
   responsiveVoice.speak(reverseWord);
 }
 
+//checks the user's answer - whether or not itt is uppercase
 function guessWord(word) {
   currentAnswer = word.toLowerCase();
 }
