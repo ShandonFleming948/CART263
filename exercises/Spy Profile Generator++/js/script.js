741b83aa4b0364d5f3a207ba9cb4c2c2f61d0cf1
ShandonFleming948 <shandonflem@gmail.com>
2021-02-11 13:25:24 -0400

    set up draw function

--- exercises/Spy Profile Generator++/js/script.js
+++ exercises/Spy Profile Generator++/js/script.js
@@ -8,6 +8,13 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`
+};
+
 /**
 Description of preload
 */
@@ -20,7 +27,9 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  spyProfile.name = prompt(`Agent, enter your name here`);
 }
 
 
@@ -28,5 +37,13 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  push();
+  textFont(`Courier, monospace`);
+  textSize(20);
+  textAlign(CENTER);
+  fill(29, 224, 32);
+  text(spyProfile.name, 100, 100);
+  pop();
 }
