1ba4cf8b93ba3b3ca332a2651213b312d41c9894
ShandonFleming948 <shandonflem@gmail.com>
2021-02-17 22:19:55 -0400

    Created handpose model

--- exercises/BubblePopper++/js/script.js
+++ exercises/BubblePopper++/js/script.js
@@ -8,12 +8,27 @@ author, and this description to match your project!
 
 "use strict";
 
+//stores the user's webcam
+let video = undefined;
+//the handpose model
+let handpose = undefined;
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(640, 480);
 
+  //accesses the user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //load the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function () {
+    console.log(`Model loaded.`);
+  });
 }
 
 
