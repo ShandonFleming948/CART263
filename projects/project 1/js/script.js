6cc46292639be14ae8ff77ce98c5423b117452b7
ShandonFleming948 <shandonflem@gmail.com>
2021-02-20 19:51:32 -0400

    Experimented with replicating the glowing orbs

--- projects/project 1/js/script.js
+++ projects/project 1/js/script.js
@@ -9,7 +9,7 @@ author, and this description to match your project!
 "use strict";
 
 let backgroundColor = {
-  h: 200,
+  h: 284,
   s: 100,
   b: 0
 };
@@ -36,9 +36,18 @@ Description of draw()
 */
 function draw() {
   background(0, 0, 0);
+
   colorMode(HSB);
   fill(backgroundColor.h, backgroundColor.s, backgroundColor.b);
-  ellipse(width / 2, height / 2, 100);
+  ellipse(200, 360, 50);
   backgroundColor.b = map(sin(angle), -1, 1, 20, 100);
-  angle += 0.05;
+  angle += 0.02;
+
+  colorMode(HSB);
+  fill(backgroundColor.h, backgroundColor.s, backgroundColor.b);
+  ellipse(200, 460, 50);
+  backgroundColor.b = map(sin(angle), -1, 1, 20, 100);
+  angle += 0.02;
+
+
 }
