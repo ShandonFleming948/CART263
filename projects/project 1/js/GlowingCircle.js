876394578bed29a343fb37741b087928877e0c00
ShandonFleming948 <shandonflem@gmail.com>
2021-02-26 21:44:20 -0400

    P1: Added final final comments :)

--- projects/project 1/js/GlowingCircle.js
+++ projects/project 1/js/GlowingCircle.js
@@ -1,5 +1,6 @@
 class GlowingCircle {
   constructor(x, y){
+    //the properties of the glowingCircles
     this.x = x;
     this.y = y;
     this.radius = 21;
@@ -14,9 +15,11 @@ class GlowingCircle {
       this.active = true;
   }
   display(){
+    //display the glowingCircles
     colorMode(HSB);
     fill(this.backgroundColor.h, this.backgroundColor.s, this.backgroundColor.b);
     ellipse(this.x, this.y, this.radius);
+    //make the glowing circles pulse from dark to light
     this.backgroundColor.b = map(sin(this.angle), -1, 1, 20, 100);
     this.angle += this.angleSpeed;
   }
