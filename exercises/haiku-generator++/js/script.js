4f163c23819fed2920e2b771c0350bc9fbb1e2f5
ShandonFleming948 <shandonflem@gmail.com>
2021-03-11 22:11:01 -0400

    E5: set up the five and seven syllable lines

--- exercises/haiku-generator++/js/script.js
+++ exercises/haiku-generator++/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Haiku-Generator++
+Shandon Fleming
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,4 +8,23 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+let fiveSyllableLines = [
+  'O, what an event',
+  'Long before the dawn',
+  'What will happen next?',
+  'Now it is all up',
+  'Where will we go then?'
+];
+
+let sevenSyllableLines = [
+  'What will be done is foreign',
+  'Do not give in to the doubt',
+  'All you have ever gone through',
+  'Hindsight is twenty twenty',
+  'Do not live a basic life'
+];
+
+
+function random(array){
+  
+}
