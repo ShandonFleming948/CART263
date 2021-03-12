7346685e44b94a65636f250189d24875dc324251
ShandonFleming948 <shandonflem@gmail.com>
2021-03-11 23:13:47 -0400

    E5: organized the display for the lines and added a fade out

--- exercises/haiku-generator++/js/script.js
+++ exercises/haiku-generator++/js/script.js
@@ -29,6 +29,45 @@ let line1 = random(fiveSyllableLines);
 let line2 = random(sevenSyllableLines);
 let line3 = random(fiveSyllableLines);
 
+let line1P = document.getElementById('line-1');
+let line2P = document.getElementById('line-2');
+let line3P = document.getElementById('line-3');
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener('click', lineClicked);
+line2P.addEventListener('click', lineClicked);
+line3P.addEventListener('click', lineClicked);
+
+
+function lineClicked(event) {
+  setNewLine(event.target);
+}
+
+function fadeOut(element, opacity) {
+  opacity -= 0.01;
+  element.style['opacity'] = opacity;
+  if (opacity > 0) {
+    requestAnimationFrame(function() {
+    fadeOut(element, opacity);
+    };
+  }
+  else {
+    //when it is faded out
+  }
+}
+
+function setNewLine(element) {
+  if (element === line1P || element === line3P) {
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if (element === line2P) {
+    element.innerText = random(sevenSyllableLines);
+  }
+}
+
 
 function random(array){
   let index = Math.floor(Math.random() * array.length);
