66b16d0d04cadbcc717caad3b5e8bd53cf04f1ad
ShandonFleming948 <shandonflem@gmail.com>
2021-03-18 23:44:30 -0400

    E6: added comments

--- exercises/Raving-Redactionist++/js/script.js
+++ exercises/Raving-Redactionist++/js/script.js
@@ -8,18 +8,22 @@ author, and this description to match your project!
 
 "use strict";
 
+// once the text is clicked, the revealed text will be hidden
 $('.top-secret').on('click', redact);
 setInterval(revelation, 2000);
 
+//hides the text
 function redact(event) {
   $(this).removeClass('revealed');
   $(this).addClass('redacted').css("display","none").fadeIn(7000);
 }
 
+//reveals the text
 function revelation() {
   $('.redacted').each(attemptReveal);
 }
 
+//pick the text at random to reveal
 function attemptReveal() {
   let r = Math.random();
   if (r < 0.1) {
