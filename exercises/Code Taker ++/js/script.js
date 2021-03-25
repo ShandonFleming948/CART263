966045087f74eee920ef85a025b9bc8f261d3106
ShandonFleming948 <shandonflem@gmail.com>
2021-03-25 13:53:10 -0400

    E7: Changed the poem and the hidden message

--- exercises/Code Taker ++/js/script.js
+++ exercises/Code Taker ++/js/script.js
@@ -8,6 +8,18 @@ author, and this description to match your project!
 
 "use strict";
 
+alert("Scan over the poem to and use the highlighted letters to reveal a secret message.");
+
+
+$('#solved-dialog').dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function(){
+      $(this).dialog('close');
+    }
+  }
+});
+
 $('.secret').on('mouseover', function(event) {
   $(this).addClass('found', 550);
   $(this).draggable({
@@ -15,11 +27,15 @@ $('.secret').on('mouseover', function(event) {
   });
 });
 
-$(`answer`).droppable({
+$(`#answer`).droppable({
   drop: function(event, ui) {
     let letter = ui.draggable.text();
     $(this).append(letter);
     ui.draggable.draggable('disable');
     ui.draggable.removeClass('found');
+    //check if the user got the correct answer
+    if ($(this).text() === 'Theremin') {
+      $('#solved-dialog').dialog('open');
+    }
   }
 });
