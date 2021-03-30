dac08b4902e3ae5693af2c430316ec9556ae0735
ShandonFleming948 <shandonflem@gmail.com>
2021-03-30 14:03:08 -0400

    P2P: Moved the images to the script and set up the display

--- projects/project 2 prototype/js/script.js
+++ projects/project 2 prototype/js/script.js
@@ -8,12 +8,17 @@ author, and this description to match your project!
 
 "use strict";
 
+let greenballImage;
+let redballImage;
+let blueballImage;
 
 /**
 Description of preload
 */
 function preload() {
-
+  greenballImage = loadImage("assets/images/greenball.jpg");
+  redballImage = loadImage("assets/images/redball.png");
+  blueballImage = loadImage("assets/images/blueball.jpg");
 }
 
 
@@ -21,7 +26,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(1250,600);
 }
 
 
@@ -29,5 +34,9 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(250);
 
+  image(greenballImage,0,0,100,100);
+  image(redballImage,400,0,100,100);
+  image(blueballImage,800,0,100,100);
 }
